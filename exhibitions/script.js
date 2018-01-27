4adcd01ec42d95b432294c0c46dc644847fc84b1
Andrea Benedetti <benedetti.a93@gmail.com>
2018-01-27 14:58:46 +0100

    exhibit page

---
+++ exhibitions/script.js
@@ -0,0 +1,62 @@
+$('.description').hide();
+
+$('.titles').hover(function(){
+	$('.description').show(200);
+}, function() {
+	$('.description').hide(200);
+})
+
+$('#group01').hover(function(){
+	$('.content').text('Think Inside The Box is an interactive installation about the topic of gender inequality. It allows people discover concretely how males and females are different. The aim of this project is to actively engage people to let them experience physically the differences from both the male and female perspectives also collecting data from them. We want to inform users providing them the most relevant data from our resesearch in an easy understandable way and also make them think about it.');
+	$('.team').text('Chiara Dardani, Zara Elmi, Marta Morandi, Sina Ranjbar, Natalia Rosso');
+}, function() {
+    $('.content').text('');
+    $('.team').text('');
+});
+
+$('#group02').hover(function(){
+	$('.content').text('The Driverless Race is a data-based game that will let up to three players to compete against each other as companies in the self-driving cars game. The aim of the game will be to dominate the self-driving cars market through strategic choices while facing obstacles, ethical issues and dynamics that are inevitably encountered during this stage of evolution of the technology. Companies networks, newspaper events and other significant data will be integrated into the game dynamics and narration.');
+	$('.team').text('Barbara Bonadies, Giulia Cosentino, Katarina Radovic, Katherin Martinez, Giovan Battista Pesarini, Katarina Radovic');
+}, function() {
+    $('.content').text('');
+    $('.team').text('');
+});
+
+$('#group03').hover(function(){
+	$('.content').text('');
+	$('.team').text('Maria Ludovica Forziano, Xue He, Yunxing Liu, Chiara Piva, Ginevra Terenghi ');
+}, function() {
+    $('.content').text('');
+    $('.team').text('');
+});
+
+$('#group04').hover(function(){
+	$('.content').text('Universal Basic Income has been a seminal concept and inspiration for many politicians, social reformer and now technologists. In the current historical conjuncture the debate is constantly growing driven by the economical crisis and technological development as well as a generalized social tension for a better work-life balance. This website is the online extension of the interactive exhibition held at Polimi. We are seeking to provide deeper knowledge about the worldwide panorama of activities towards UBI and challenge the visitors to project themselves in the scenario of widespread basic income adoption. The installation is a platform for engaging the public on the personal and ethical issue of rethinking the use and the meaning of the resources the Basic Income adoption. It aims to build a contextual dataset for answering the question: "If the people had not to work for a living, how will they choose to spend their resources and time?" ');
+	$('.team').text('Matteo Testa, Matteo Sacchi, Irina Kasatkina, Giorgio Falvo, Nicolas Attolico ');
+}, function() {
+    $('.content').text('');
+    $('.team').text('');
+});
+
+$('#group05').hover(function(){
+	$('.content').text('Hate speech and freedom of expression: are these conflicting concepts or complementary ideas? Must the hatred be granted free, or should freedom be restricted? As Karl Popper stated in 1945 expressing the so-called “Paradox of Tolerance”, a society should be intolerant towards intolerance, in order to keep a tolerant system. Is this true? The aim of this interactive analysis is to give the participants an active role, leaving them the possibility to decide what, in their opinion, should be the answer: they are going to be the censorship, choosing between transparency or safety. ');
+    $('.team').text('Silvia Castagna, Maria Girasoli, Davide Modanese, Marianna Piazza, Caterina Scarioni ');
+}, function() {
+	$('.content').text('');
+    $('.team').text('');
+});
+
+$('#group06').hover(function(){
+	$('.content').text('Nuclear waste is the material that nuclear fuel becomes after it is used in a reactor. The exhibition showcase is a quiz board game of the "nuclear waste" theme. Nuclear waste is a Damocles for the whole world, also, it‘s an attractive and less public issue, we hope that through such interaction and popular science can lead the public to think about nuclear energy issues. ');
+    $('.team').text('Manli Zhu, Simone Casartelli, Xiaoxi Huang, Xuechun Zhao, Yue Qiu');
+}, function() {
+    $('.content').text('');
+    $('.team').text('');
+});
+
+$('#group07').hover(function(){
+	$('.content').text(' The government of North Korea is denying the existence of labour camps and lifelong prisons for years. Political, economic and social dissident are sent to those facilities. Escaping is the only way of getting out from the country, who is caught has to face terrible conditions. Who managed to escaped talked about tortures, human rights violations and abuses inside government facilities hidden in the middle of nowhere. Testimonies of violence inside the labour camps are often deemed to be liars both inside and outside their country. Where is the truth? "Those who are sent to the mountains" exhibition will try to tell the zstories of the ones that managed to escape, along with the ones who did not.');
+    $('.team').text('Elena Aversa, Alessandra Bufano, Yi Cui, Xiu Huiting, Francesca Morini, Yidan Sun ');
+}, function() {
+	$('.content').text('');
+    $('.team').text('');});
